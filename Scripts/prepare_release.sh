a65c54fa81b4d875ceed96032c29da9d79b86c5f
Chase Colman <chase@colman.io>
2015-06-24 19:54:19 -0400

    Fix some typographical errors

--- Scripts/prepare_release.sh
+++ Scripts/prepare_release.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 shopt -s extglob
-git rm -f *.zip
-for f in Monoid-Regular*.ttf; do
+for f in ./Monoid-Regular*.ttf; do
     # Get the built options
     options=${f#Monoid-Regular}
     # Add all the fonts with those options
