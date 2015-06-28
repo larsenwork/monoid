9fb2c9e7a8927ed8caa5740a89f51c5388dfe0c6
Chase Colman <chase@colman.io>
2015-06-28 14:37:13 -0400

    Fix release build

--- Scripts/prepare_release.sh
+++ Scripts/prepare_release.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 shopt -s extglob
-for f in ./Monoid-Regular*.ttf; do
+git rm -f ./*.zip || echo "No releases"
+cd _release
+for f in Monoid-Regular*.ttf; do
     # Get the built options
     options=${f#Monoid-Regular}
+    echo "Options: ${options}"
     # Add all the fonts with those options
     zip -j "../Monoid${options%.ttf}.zip" Monoid-+([^-])$options
 done
+cd ..
+#sudo rm -rf _release
