cc08b897f0907f35a81bbf347959910f381d2794
Chase Colman <chase@colman.io>
2015-07-12 22:37:32 -0400

    Fix Readme+License adding

--- Scripts/prepare_release.sh
+++ Scripts/prepare_release.sh
@@ -3,7 +3,7 @@ shopt -s extglob
 
 # Copy artifacts to keep after branch switch
 cp Scripts/gh-pages.sh /tmp
-cp Source/Readme+License.pdf /tmp
+cp Source/Readme+License.html /tmp
 
 # Switch to release branch
 git checkout release
@@ -16,7 +16,7 @@ for f in Monoid-Regular*.ttf; do
     options=${f#Monoid-Regular}
     echo "Options: ${options}"
     # Add all the fonts with those options
-    zip -j "../Monoid${options%.ttf}.zip" Monoid-+([^-])$options /tmp/Readme+License.pdf
+    zip -j "../Monoid${options%.ttf}.zip" Monoid-+([^-])$options /tmp/Readme+License.html
 done
 cd ..
 sudo rm -rf _release
