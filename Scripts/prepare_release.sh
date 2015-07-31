f1ca9f333397d91abcb38592e331106273f0aa2a
Andreas Larsen <andreas@larsenwork.com>
2015-07-31 16:22:08 +0200

    Removing Monoisome from script + new images

--- Scripts/prepare_release.sh
+++ Scripts/prepare_release.sh
@@ -4,7 +4,6 @@ shopt -s extglob
 # Copy artifacts to keep after branch switch
 cp Scripts/gh-pages.sh /tmp
 cp Source/Readme+License.html /tmp
-cp Monoisome/Monoisome-Regular.ttf /tmp
 
 # Switch to release branch
 git checkout release
@@ -15,7 +14,7 @@ for f in Monoid-Regular*.ttf; do
     options=${f#Monoid-Regular}
     echo "Options: ${options}"
     # Add all the fonts with those options
-    zip -j "../Monoid${options%.ttf}.zip" Monoid-+([^-])$options /tmp/Readme+License.html tmp/Monoisome-Regular.ttf
+    zip -j "../Monoid${options%.ttf}.zip" Monoid-+([^-])$options /tmp/Readme+License.html
 done
 cd ..
 sudo rm -rf _release
--- Utilities/Images/Readme.md
+++ Utilities/Images/Readme.md
@@ -1,5 +1,7 @@
 Use these for blogs etc...
 
+![image](https://github.com/andreaslarsen/monoid/raw/master/Utilities/Images/MonoidReadme.png)
+![image](https://github.com/andreaslarsen/monoid/raw/master/Utilities/Images/Monoisome.png)
 ![image](https://github.com/andreaslarsen/monoid/raw/master/Utilities/Images/Monoid1.png)
 ![image](https://github.com/andreaslarsen/monoid/raw/master/Utilities/Images/Monoid2.png)
 ![image](https://github.com/andreaslarsen/monoid/raw/master/Utilities/Images/Monoid3.png)
