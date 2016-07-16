375fcdb6417ea1788d50791f61c3c28c11864f09
Chase Colman <chase@colman.io>
2016-07-16 18:47:06 -0400

    Use a blacklist for change checks and re-enable
    Sorry for being out of touch, this should make it simpler to manage
    
    Instead of making you add the files/folders that you WANT to check,
    now the check script will exclude files that you don't want to check for
    changes .
    
    To add files you don't want to check, just add file/folder names between
    the parentheses, like:
    ```
    DO_NOT_CHECK=(
         Readme.md
         "A File With Spaces.txt"
         Junk
    )
    ```

--- Scripts/check-for-changes.sh
+++ Scripts/check-for-changes.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-# Check if there were any changes in the Source directory
-! git diff `echo "$CIRCLE_COMPARE_URL" | cut -d '/' -f 7` --name-only --exit-code Source Scripts circle.yml
+# Do not check these directories or files for changes:
+DO_NOT_CHECK=(
+	Readme.md
+)
+
+# Get the top-level file list
+check=`ls -x`
+# Remove DO_NOT_CHECK from CHECK
+for i in ${DO_NOT_CHECK[@]}; do
+	check=${check/ ${i} / }
+done
+# Put into an array
+check=( $check )
+
+# Check if there were any changes since the last commit
+! git diff `echo "$CIRCLE_COMPARE_URL" | cut -d '/' -f 7` --name-only --exit-code ${check[*]}
